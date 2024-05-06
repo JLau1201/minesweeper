cb9273f1587f36b388e7983f2835c798535cc64b
Jerome Lau <jeromelau1201@gmail.com>
2024-05-05 23:00:42 -0700

    minesweeper

--- src/App.js
+++ src/App.js
@@ -1,135 +1,253 @@
 import React, {useState} from 'react';
-function Square({value, handleMouseDown}){
+
+const difficulties = [
+    {size: { rows: 8, cols: 8 }, bombs: 10},
+    {size: { rows: 16, cols: 16 }, bombs: 40},
+    {size: { rows: 16, cols: 30 }, bombs: 99}
+];
+
+const offsets = [
+    {row: -1, col: -1}, // top-left
+    {row: -1, col: 0},  // top
+    {row: -1, col: 1},  // top-right
+    {row: 0, col: -1},  // left
+    {row: 0, col: 1},   // right
+    {row: 1, col: -1},  // bottom-left
+    {row: 1, col: 0},   // bottom
+    {row: 1, col: 1}    // bottom-right
+];
+
+function Square({value, handleMouseDown, i, j, clickedSquares}){
     const handleContextMenu = (e) => {
         e.preventDefault(); 
     };
 
+    const renderTextColor = () => {
+        switch(value) {
+            case 'F':
+                return "flag";
+            case -1:
+                return "bomb";
+            case 0:
+                return "zero";
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            default:
+                return "";
+        }
+    }
+
     return (
-        <button className="square" onContextMenu={handleContextMenu} onMouseDown={handleMouseDown}>
+        <button 
+            className={`${clickedSquares[i][j] ? 'square' : 'mask'} ${renderTextColor()}`}
+            onContextMenu={handleContextMenu} 
+            onMouseDown={handleMouseDown}
+        >
             {value}
         </button>
     );
 }
 
-function generateBoard(){
-    const board = [...Array(8)].map(e => Array(10).fill(0));
+function generateBoard(diff){
+    const {size, bombs} = difficulties[diff];
+    const {rows, cols} = size;
 
-    for (let i = 0; i < 10; i++) {
-        let x, y;
-        do {
-            x = Math.floor(Math.random() * 8);
-            y = Math.floor(Math.random() * 10);
-        } while (board[x][y] === -1); 
-        board[x][y] = -1;
+    const board = [...Array(rows)].map(e => Array(cols).fill(0));
+
+    let bombsPlaced = 0;
+    while (bombsPlaced < bombs) {
+        const x = Math.floor(Math.random() * rows);
+        const y = Math.floor(Math.random() * cols);
+        if (board[x][y] !== -1) {
+            board[x][y] = -1;
+            bombsPlaced++;
+        }
     }
-
-    const offsets = [
-        {row: -1, col: -1}, // top-left
-        {row: -1, col: 0},  // top
-        {row: -1, col: 1},  // top-right
-        {row: 0, col: -1},  // left
-        {row: 0, col: 1},   // right
-        {row: 1, col: -1},  // bottom-left
-        {row: 1, col: 0},   // bottom
-        {row: 1, col: 1}    // bottom-right
-    ];
   
-    for (let i = 0; i < 8; i++){
-        for(let j = 0; j < 10; j++){
+    for (let x = 0; x < rows; x++){
+        for(let y = 0; y < cols; y++){
             let tileValue = 0;
-            if(board[i][j] !== -1){
-                for (let k = 0; k < offsets.length; k++){
-                    const newRow = i + offsets[k].row;
-                    const newCol = j + offsets[k].col;
-                    if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 10) {
+            if(board[x][y] !== -1){
+                for (let i = 0; i < offsets.length; i++){
+                    const newRow = x + offsets[i].row;
+                    const newCol = y + offsets[i].col;
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
                         if (board[newRow][newCol] === -1) {
                             tileValue++;
                         }
                     }
                 }
-                board[i][j] = tileValue;
+                board[x][y] = tileValue;
             }
         }
     }
     return board;
 }
 
-function Board({squares, setSquares, gameBoard}){
-    function flood(x, y, visited, nextSquares){
-        const offsets = [
-            {row: 0, col: 0},   // center
-            {row: -1, col: -1}, // top-left
-            {row: -1, col: 0},  // top
-            {row: -1, col: 1},  // top-right
-            {row: 0, col: -1},  // left
-            {row: 0, col: 1},   // right
-            {row: 1, col: -1},  // bottom-left
-            {row: 1, col: 0},   // bottom
-            {row: 1, col: 1}    // bottom-right
-        ];
+function Board({squares, setSquares, gameBoard, diff}){
+    const {size, bombs} = difficulties[diff];
+    const {rows, cols} = size;
 
-        visited.add(`${x},${y}`);
-    
+    const [clickedSquares, setClickedSquares] = useState([...Array(rows)].map(e => Array(cols).fill(false)));
+    const [gameOver, setGameOver] = useState(false);
+
+    function countClickedSquares() {
+        let count = 0;
+        for (let i = 0; i < rows; i++){
+            for (let j = 0; j < cols; j++){
+                if(clickedSquares[i][j] === true){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    function flood(x, y, nextSquares, nextClickedSquares){
+        
+        nextClickedSquares[x][y] = true;
 
         for(let i = 0; i < offsets.length; i++){
             const newX = x + offsets[i].row;
             const newY = y + offsets[i].col;
-            const key = `${newX},${newY}`;
 
-            if(newX >= 0 && newX < 8 && newY >= 0 && newY < 10 && !visited.has(key)){
-                if(gameBoard.gameBoard[newX][newY] === 0){
-                    flood(newX, newY, visited, nextSquares);
+            if(newX >= 0 && newX < rows && newY >= 0 && newY < cols && nextClickedSquares[newX][newY] === false && nextSquares[newX][newY] !== 'F'){
+                nextSquares[newX][newY] = gameBoard[newX][newY];
+                nextClickedSquares[newX][newY] = true;
+
+                if(gameBoard[newX][newY] === 0){
+                    flood(newX, newY, nextSquares, nextClickedSquares);
                 }
-                nextSquares[newX][newY] = gameBoard.gameBoard[newX][newY];
             }
         }
         return;
     }
 
-    function handleMouseDown(x, y, e){
-        const nextSquares = squares.slice();
-        switch(e.button){
-            case 0:
-                if(gameBoard.gameBoard[x][y] === -1){
-                    console.log("You Lose!");
-                    nextSquares[x][y] = gameBoard.gameBoard[x][y];
-                }else if(nextSquares[x][y] !== 'F'){
-                    if(gameBoard.gameBoard[x][y] === 0){
-                        const visited = new Set();
-                        flood(x, y, visited, nextSquares);
+    function handleEndGame(state, nextSquares, nextClickedSquares){
+        if(state === -1){
+            for(let x = 0; x < rows; x++){
+                for(let y = 0; y < cols; y++){
+                    if(gameBoard[x][y] === -1){
+                        nextSquares[x][y] = gameBoard[x][y];
+                        nextClickedSquares[x][y] = true;
                     }
-                    nextSquares[x][y] = gameBoard.gameBoard[x][y];
                 }
-            break;
-            case 1:
+            }
 
-            break;
-            case 2:
-                if(nextSquares[x][y] === null){
-                    nextSquares[x][y] = 'F';
-                }else if(nextSquares[x][y] === 'F'){
-                    nextSquares[x][y] = null;
-                }
-
-            break;
-            default:
-                console.log("Unknown mouse button clicked");
-            break;
+            setGameOver(true);
+            console.log('You Lose!')
+        }else if(countClickedSquares() === rows * cols - bombs){
+            setGameOver(true);
+            console.log("You Win!")
         }
-        
-        setSquares(nextSquares);
+    }
+
+    function handleMouseDown(x, y, e){
+        if(!gameOver){
+            const nextSquares = squares.slice();
+            const nextClickedSquares = clickedSquares.slice();
+
+            switch(e.button){
+                case 0:
+                    if(nextSquares[x][y] === null){
+                        if(gameBoard[x][y] === -1){
+                            handleEndGame(-1, nextSquares, nextClickedSquares);
+                            nextClickedSquares[x][y] = true;
+                        }else if(gameBoard[x][y] === 0){
+                            flood(x, y, nextSquares, nextClickedSquares);
+                        }else{
+                            nextClickedSquares[x][y] = true;
+                        }
+                        handleEndGame(0, nextSquares, nextClickedSquares);
+
+                        nextSquares[x][y] = gameBoard[x][y];
+                    }
+                break;
+                case 1:
+                    if(nextClickedSquares[x][y] === true){
+                        let numFlags = 0;
+                        for (let i = 0; i < offsets.length; i++){
+                            const newX = x + offsets[i].row;
+                            const newY = y + offsets[i].col;
+                            if(newX >= 0 && newX < rows && newY >= 0 && newY < cols){
+                                if(nextSquares[newX][newY] === 'F'){
+                                    numFlags++;
+                                }
+                            }
+                        }
+                        if(numFlags === nextSquares[x][y]){
+                            for(let i = 0; i < offsets.length; i++){
+                                const newX = x + offsets[i].row;
+                                const newY = y + offsets[i].col;
+                                if(newX >= 0 && newX < rows && newY >= 0 && newY < cols){
+                                    if(nextSquares[newX][newY] === null){
+                                        if(gameBoard[newX][newY] === -1){
+                                            handleEndGame(-1, nextSquares, nextClickedSquares);
+                                            nextClickedSquares[newX][newY] = true;
+                                        }else if(gameBoard[newX][newY] === 0){
+                                            flood(newX, newY, nextSquares, nextClickedSquares);
+                                        }else{
+                                            nextClickedSquares[newX][newY] = true;
+                                        }
+                                        handleEndGame(0, nextSquares, nextClickedSquares);
+                
+                                        nextSquares[newX][newY] = gameBoard[newX][newY];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                break;
+                case 2:
+                    if(nextSquares[x][y] === null){
+                        nextSquares[x][y] = 'F';
+                    }else if(nextSquares[x][y] === 'F'){
+                        nextSquares[x][y] = null;
+                    }
+
+                break;
+                default:
+                    console.log("Unknown mouse button clicked");
+                break;
+            }
+            setClickedSquares(clickedSquares);
+            setSquares(nextSquares);
+        }
     }
 
     function renderBoard(){
         const board = [];
-        for(let i = 0; i < 8; i++){
+        for(let x = 0; x < rows; x++){
             const boardRow = [];
-            for (let j = 0; j < 10; j++){
-                const index = i * 8 + j;
-                boardRow.push(<Square key={index} value={squares[i][j]} handleMouseDown={(e) => handleMouseDown(i, j, e)}/>);
+            for (let y = 0; y < cols; y++){
+                const index = x * rows + y;
+                boardRow.push(
+                    <Square 
+                        key={index} 
+                        value={squares[x][y]} 
+                        handleMouseDown={(e) => handleMouseDown(x, y, e)}
+                        i={x}
+                        j={y}
+                        clickedSquares={clickedSquares}
+                    />
+                );
             }
             board.push(
-                <div className="board-row" key={i}>
+                <div className="board-row" key={x}>
                     {boardRow}
                 </div>);
         }
@@ -144,21 +262,22 @@ function Board({squares, setSquares, gameBoard}){
     );
 }
 
-function Game(gameBoard){
-    const [squares, setSquares] = useState([...Array(8)].map(e => Array(10).fill(null)));
+function Game({gameBoard, diff}){
+    const [squares, setSquares] = useState([...Array(difficulties[diff].size.rows)].map(e => Array(difficulties[diff].size.cols).fill(null)));
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board squares={squares} setSquares={setSquares} gameBoard={gameBoard}/>
+                <Board squares={squares} setSquares={setSquares} gameBoard={gameBoard} diff={diff}/>
             </div>
         </div>
     );
 }
 
 function Main(){
-    const gameBoard = generateBoard();
-
-    return (<Game gameBoard={gameBoard}/>)
+    const diff = 2;
+    const gameBoard = generateBoard(diff);
+    return (<Game gameBoard={gameBoard} diff={diff}/>)
 }
 
 export default Main;
